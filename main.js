4858b846979a76b83523c06b2535955af3bc4a2e
Varannchik <varann@inbox.ru>
2019-05-30 16:18:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
 let btn=document.querySelector('.btn');
-    vr=document.querySelector('.road_vertical');
-    gr=document.querySelector('.road_gorizontal');
-    vertLeft=document.querySelector('.vert_svet_left');
-    vertRight=document.querySelector('.vert_svet_right');
-    gorLeft=document.querySelector('.gorizont_svet_left');
-    gorRight=document.querySelector('.gorizont_svet_right');
-
-   
-
-    btn.addEventListener('click',function(){
+    btn.addEventListener('click',function(){             
+        vr=document.querySelector('.road_vertical');
+        gr=document.querySelector('.road_gorizontal');
+        vert=document.querySelectorAll('.vert_svet');        
+        gor=document.querySelectorAll('.gorizont_svet');
         if(this.classList.contains('active')){
             vr.style.background = "#00ff00";
             gr.style.background='#df0024';
             this.classList.remove('active');
-            vertLeft.querySelector('.svet_green').style.background = "#00ff00";
-            vertRight.querySelector('.svet_green').style.background = "#00ff00";
-            vertLeft.querySelector('.svet_red').style.background = "transparent";
-            vertRight.querySelector('.svet_red').style.background = "transparent";
-            gorLeft.querySelector('.svet_red').style.background='#df0024';
-            gorRight.querySelector('.svet_red').style.background='#df0024';
-            gorLeft.querySelector('.svet_green').style.background='transparent';
-            gorRight.querySelector('.svet_green').style.background='transparent';
-            
+            for(let i=0; i<=vert.length; i++){
+                vert[i].querySelector('.svet_green').style.background = "#00ff00";
+                gor[i].querySelector('.svet_red').style.background = "#df0024";
+                vert[i].querySelector('.svet_red').style.background = "transparent";
+                gor[i].querySelector('.svet_green').style.background = "transparent";
+            } 
         }else{
             vr.style.background='#df0024';
             gr.style.background='#00ff00';//zel
             this.classList.add('active');
-            vertLeft.querySelector('.svet_green').style.background = "transparent";
-            vertRight.querySelector('.svet_green').style.background = "transparent";
-            vertLeft.querySelector('.svet_red').style.background = "#df0024";
-            vertRight.querySelector('.svet_red').style.background = "#df0024";
-            gorLeft.querySelector('.svet_red').style.background='transparent';
-            gorRight.querySelector('.svet_red').style.background='transparent';
-            gorLeft.querySelector('.svet_green').style.background='#00ff00';
-            gorRight.querySelector('.svet_green').style.background='#00ff00';
-        }
-    
+            for(let i=0; i<=vert.length; i++){
+                vert[i].querySelector('.svet_red').style.background = "#df0024";
+                gor[i].querySelector('.svet_green').style.background = "#00ff00";
+                vert[i].querySelector('.svet_green').style.background = "transparent";
+                gor[i].querySelector('.svet_red').style.background = "transparent";
+            } 
+        } 
     });    
